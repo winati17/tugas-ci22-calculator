8a900cfa166d8e3ac5efeab3953f11e51cdc9d72
winati17 <winatimn@gmail.com>
2023-07-14 00:56:05 +0800

    first commit

---
+++ git.c
@@ -0,0 +1,11 @@
+/*
+utk configuration :
+git config --global user.name winati17
+git config --global user.email winatimn@gmail.com
+git config --global color.ui auto  
+
+git init -> utk memasukkan directory ke dlm github
+rmdir .git -> utk menghapus directorynya dri github (?)
+
+
+*/
